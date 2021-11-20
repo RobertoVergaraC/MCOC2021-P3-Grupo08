9cfd2e904a12d86323eca74dd529f828a6bfd496
RobertoVergaraC <ravergara@miuandes.cl>
2021-11-19 22:08:32 -0300

    Entrega 5 código LISTO

--- Entrega 5/p3e5.py
+++ Entrega 5/p3e5.py
@@ -13,19 +13,6 @@ zonas_gdf = gps.read_file("eod.json")
 ox.config(use_cache=True, log_console=True)
 G = nx.read_gpickle("Santiago_Grueso.gpickle")
 
-# for i, edge in enumerate(G.edges):
-# 	if "geometry" in G.edges[edge[0], edge[1], 0]:
-# 		if G.edges[edge[0], edge[1], 0]["geometry"] == np.nan:
-# 			G.edges[edge[0], edge[1], 0]["geometry"] = LineString([(G.nodes[edge[0]]["x"],G.nodes[edge[0]]["y"]),(G.nodes[edge[1]]["x"],G.nodes[edge[1]]["y"])])
-# 		else:
-# 			G.edges[edge[0], edge[1], 0]["geometry"] = G.edges[edge[0], edge[1], 0]["geometry"]
-# 	else:
-# 		a = LineString([(G.nodes[edge[0]]["x"],G.nodes[edge[0]]["y"]),(G.nodes[edge[1]]["x"],G.nodes[edge[1]]["y"])])
-# 		nx.set_edge_attributes(G, a, "geometry")
-
-# TRANSFORMAMOS A GDF
-gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
-
 # csv file name
 filename = "mod2.csv"
   
@@ -38,26 +25,73 @@ with open(filename, 'r') as csvfile:
 
 Matriz_OD={}
 for i in rows:
-	Matriz_OD[(int(i[0]), int(i[1]))] = float(i[2])
+	if i[0] != 324 or i[1] != 324:
+		Matriz_OD[(int(i[0]), int(i[1]))] = float(i[2])
 
 zonas_seleccionadas_id = []
 for key in Matriz_OD:
 	zonas_seleccionadas_id.append(key[0])
 	zonas_seleccionadas_id.append(key[1])
 
+zonas_seleccionadas_id = sorted(set(zonas_seleccionadas_id))
+zonas_seleccionadas_id.remove(324)
+
 zonas_seleccionadas = zonas_gdf[zonas_gdf.ID.isin(zonas_seleccionadas_id)]
 
-#gdf_edges_seleccionados = gps.clip(gdf_edges, zonas_seleccionadas)
-# print(gdf_edges_seleccionados)
+# TRANSFORMAMOS A GDF
+gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
 
+print(f"Cantidad de Zonas: {len(zonas_seleccionadas_id)}")
+
+viajes_totales = 0
+for key in Matriz_OD:
+	viajes_totales += float(Matriz_OD[key]) 
+
+print(f"Cantidad de Viajes: {viajes_totales}")
+
+#ALGORITMO PARA SABER ZONA DAÑADA = 324
+# contador = 0
+# a = []
+# NODOS_MALOS = []
+# while contador<len(zonas_seleccionadas_id):
+# 	a.append(zonas_seleccionadas_id[contador])
+# 	zonas_seleccionadas = zonas_gdf[zonas_gdf.ID.isin(a)]
+# 	try:
+# 		gdf_edges_seleccionados = gps.clip(gdf_edges, zonas_seleccionadas)
+# 		contador+=1
+# 		print("FUNCA")
+# 	except:
+# 		a.remove(zonas_seleccionadas_id[contador])
+# 		NODOS_MALOS.append(zonas_seleccionadas_id[contador])
+# 		contador+=1
+# 		print("NO FUNCA")
+
+# zonas_seleccionadas_id_final = a
+
+
+# zonas_seleccionadas = zonas_gdf[zonas_gdf.ID.isin(zonas_seleccionadas_id_final)]
+
+gdf_edges_seleccionados = gps.clip(gdf_edges, zonas_seleccionadas)
+
+#Graficamos zonas
 fig, ax = plt.subplots(1, 1)
 
 zonas_seleccionadas.plot(ax=ax, color="#CDCDCD")
 
-gdf_edges[gdf_edges.highway=="tertiary"].plot(ax=ax, color="blue", linewidth=0.5)
-gdf_edges[gdf_edges.highway=="secondary"].plot(ax=ax, color="green", linewidth=0.5)
-gdf_edges[gdf_edges.highway=="primary"].plot(ax=ax, color="yellow", linewidth=0.5)
-gdf_edges[gdf_edges.highway=="motorway"].plot(ax=ax, color="orange", linewidth=0.5)
+plt.show()
+
+#Graficamos Calles y Zonas
+plt.figure()
+ax = plt.subplot(111)
+
+zonas_seleccionadas.plot(ax=ax, color="#CDCDCD")
+
+gdf_edges_seleccionados[gdf_edges_seleccionados.highway=="motorway"].plot(ax=ax, color="orange", linewidth=0.5)
+gdf_edges_seleccionados[gdf_edges_seleccionados.highway=="primary"].plot(ax=ax, color="yellow", linewidth=0.5)
+gdf_edges_seleccionados[gdf_edges_seleccionados.highway=="secondary"].plot(ax=ax, color="green", linewidth=0.5)
+gdf_edges_seleccionados[gdf_edges_seleccionados.highway=="tertiary"].plot(ax=ax, color="blue", linewidth=0.5)
+
 gdf_edges[gdf_edges.name=="Autopista Vespucio Oriente"].plot(ax=ax, color="red", linewidth=3)
 
 plt.show()
+
